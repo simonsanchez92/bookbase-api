7ac04efb180d5532bf63497a68998ae6ae79d143
simonsanchez92 <simonsanchez.1992@gmail.com>
2024-09-18 12:13:23 -0300

    Modified BookResponseDto

--- Bookbase.Application/Dtos/Responses/BookResponseDto.cs
+++ Bookbase.Application/Dtos/Responses/BookResponseDto.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishDate { get; set; }
+        public string Description { get; set; }
+        public string CoverUrl { get; set; }
+        public int PageCount { get; set; }
     }
 }
